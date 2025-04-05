740c74d5b32b90f19af4d0c6a89a5717d393686a
huy.tran <tranvanhuyy13@gmail.com>
2025-04-05 22:49:35 +0700

    feat sum

--- Application_test/src/Test.java
+++ Application_test/src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 	public static int xx(int a,int b) {
 		return a*b;
 	}
+	
+	public static int sum(int a,int b) {
+		return a + b;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello m");
 		System.out.println("nhan m");
