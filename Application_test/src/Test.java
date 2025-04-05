117d9228f7a014cbc4fc8ecb802c6bd2bc8b8df0
huy.tran <tranvanhuyy13@gmail.com>
2025-04-05 19:38:24 +0700

    nhan method

--- Application_test/src/Test.java
+++ Application_test/src/Test.java
@@ -1,12 +1,12 @@
 
 public class Test {
 	
-	public static int plus(int a,int b) {
-		return a+b;
+	public static int xx(int a,int b) {
+		return a*b;
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello m");
-		System.out.println("bye m");
+		System.out.println("nhan m");
 		
 	}
 
