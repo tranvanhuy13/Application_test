1d7f0295d38198f573a5a8884de904a2a6dce53f
huy.tran <tranvanhuyy13@gmail.com>
2025-04-05 19:07:29 +0700

    new add method

--- Application_test/src/Test.java
+++ Application_test/src/Test.java
@@ -1,9 +1,13 @@
 
 public class Test {
-
+	
+	public static int add(int a,int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
 		System.out.print("Hello m");
-
+		System.out.print("bye m");
+		
 	}
 
 }
